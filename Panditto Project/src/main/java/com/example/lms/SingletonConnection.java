6adac2e2f0de464246bd6863541325a180cc97c8
Tamrin Nahar Lam <137845267+tamrin342@users.noreply.github.com>
2023-10-11 02:58:17 +0600

    Update SingletonConnection.java

--- Panditto Project/src/main/java/com/example/lms/SingletonConnection.java
+++ Panditto Project/src/main/java/com/example/lms/SingletonConnection.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SingletonConnection {
-    private final String DB_HOSTNAME = "localhost";
-    private final String DB_NAME = "panditto";
+    private final String DB_HOSTNAME = "**";
+    private final String DB_NAME = "***";
     private final String DB_URL = "jdbc:mysql://"+DB_HOSTNAME+"/"+DB_NAME;
-    private final String DB_USERNAME = "root";
+    private final String DB_USERNAME = "**";
     private final String DB_PASSWORD = "*****";
     private static SingletonConnection instance = new SingletonConnection();
     private static Connection connection;
